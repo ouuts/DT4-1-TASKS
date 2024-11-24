3e24715438824d47b2a07b2d0e8c639c941356d9
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 17:35:17 +0200

    Update uzd4.py

--- uzd4.py
+++ uzd4.py
@@ -1,24 +1,29 @@
-#NAV LATVIEŠU VALODĀĀĀĀ
-
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from deep_translator import GoogleTranslator
 
 nltk.download('vader_lexicon')
-
-sia = SentimentIntensityAnalyzer() 
+sia = SentimentIntensityAnalyzer()
 
 while True:
-    text = input("Ievadiet tekstu (raksti 'atā', lai pārtrauktu): ")
-    if text.lower() in ["atā", "ata"]:
+    lat_teksts = str(input("Ievadiet tekstu (raksti 'atā', lai pārtrauktu): "))
+    
+    if lat_teksts.lower() in ["atā", "ata"]:
         print("Uz redzēšanos!")
         break
-
-    sentiment_dict = sia.polarity_scores(text)
+        
+    # Translate from Latvian to English
+    eng_teksts = GoogleTranslator(source='lv', target='en').translate(lat_teksts)
+    print(f"Tulkojums uz angļu: {eng_teksts}")
+    
+    # Analyze sentiment
+    sentiment_dict = sia.polarity_scores(eng_teksts)
     compound = sentiment_dict['compound']
-
+    
     if compound >= 0.05:
-        print("Emocionālais noskaņojs: Pozitīvs")
+        print("Emocionālais noskaņojums: Pozitīvs")
     elif compound <= -0.05:
         print("Emocionālais noskaņojums: Negatīvs")
     else:
         print("Emocionālais noskaņojums: Neitrāls")
+    print()
