23d8225941f185d26999a29f4c4ed234e05a5ac7
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 23:16:14 +0200

    Update uzd5.py

--- uzd5.py
+++ uzd5.py
@@ -1,13 +1,18 @@
 import re
 
 def notiri_text(text):
-    cleaned = re.sub(r'http\S+|[@!]', '', text)
+
+    cleaned = re.sub(r"http\S+|[@!]", "", text)
     cleaned = cleaned.lower()
     return cleaned
 
 while True:
 
-    teksts = input("Ieraksti tekstu (atstāj tukšu priekš piemēra): ").strip()
+    teksts = input("Ieraksti tekstu (vai ata lai beigtu, vai atstāj tukšu piemēram): ").strip()
+    if teksts.lower() == "atā":
+        print("Uz redzēšanos!")
+        break
+
     if not teksts:
         teksts = "@John: Šis ir lielisks produkts!!! Vai ne? 👏 👏 👏 http://example.com"
         print(f"Piemērs: {teksts}")
