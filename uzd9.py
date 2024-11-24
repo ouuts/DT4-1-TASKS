6801d7a508ca5ba668b2678c55684f72fae4a56e
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 23:18:30 +0200

    Update uzd9.py

--- uzd9.py
+++ uzd9.py
@@ -3,19 +3,21 @@ from deep_translator import GoogleTranslator
 
 def generate_story(sakums):
 
-    eng_sakums = GoogleTranslator(source='lv', target='en').translate(sakums)
-    generator = pipeline('text-generation', model='gpt2')
+    eng_sakums = GoogleTranslator(source="lv", target="en").translate(sakums)
+    generator = pipeline("text-generation", model="gpt2")
     set_seed(42) 
-    generated_text = generator(eng_sakums,max_length=100,num_return_sequences=1,temperature=0.7,truncation=True,pad_token_id=generator.tokenizer.eos_token_id)[0]['generated_text']
-    lat_teksts = GoogleTranslator(source='en', target='lv').translate(generated_text)
+    generated_text = generator(eng_sakums,max_length=100,num_return_sequences=1,temperature=0.7,truncation=True,pad_token_id=generator.tokenizer.eos_token_id)[0]["generated_text"]
+    lat_teksts = GoogleTranslator(source="en", target="lv").translate(generated_text)
     return lat_teksts
 
 while True:
 
-    sakums = input("Ievadiet sākuma frāzi (vai 'ata' lai beigtu): ").strip()
-    if sakums.lower() == 'ata':
-        print("Programma beigta!")
+    sakums = input("Ievadiet sākuma frāzi (vai ata lai beigtu, vai atstāj tukšu piemēram): ").strip()
+    
+    if sakums.lower() == "atā":
+        print("Uz redzēšanos!")
         break
+        
     if not sakums:
         sakums = "Reiz kādā tālā zemē"
         print(f"Izmantojam piemēra sākuma frāzi: {sakums}")
