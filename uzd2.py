1f861450ae45857c4e9e0acc26bb06dc9fbdee58
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 23:15:26 +0200

    Update uzd2.py

--- uzd2.py
+++ uzd2.py
@@ -1,16 +1,13 @@
 from langdetect import detect
 
 while True:
-    # Lietotājs ievada tekstu
-    text = input("Ievadiet tekstu (raksti 'atā', lai pārtrauktu): ").strip()
+
+    text = input("Ievadiet tekstu (raksti atā, lai pārtrauktu): ").strip()
     
-    # Vai lietotājs grib beigt ciklu (ievadot "atā")
     if text.lower() == "atā":
-        print("atā")
+        print("Uz redzēšanos!")
         break
 
-    # Nosakām valodu, izmantojot langdetect
     detected_language = detect(text)
     
-    # Izvada valodu
     print(f"valoda: {detected_language}")
