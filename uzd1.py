6fbb6c44a3ddcbccbf52ddefe70fb37963d47bfb
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 22:08:01 +0200

    Update uzd1.py

--- uzd1.py
+++ uzd1.py
@@ -3,27 +3,19 @@ from collections import Counter
 import re
 
 def biezzums(text):
-    # .lower izdarā tā lai Kaķis un kaķis skaitas kopā
-    text = text.lower()
 
-    # izmantojam TreebankWordTokenizer
+    text = text.lower()
     tokenizer = TreebankWordTokenizer()
     words = tokenizer.tokenize(text)
-
-    # Izfiltrē punktus un komatus
     words = [word for word in words if word not in ['.', ',']]
-
-    # Saskaitā vārdu biežumu
     return dict(Counter(words))
 
-text = input("Ievadi tekstu (atstāj tukšu priekš piemēra):")
-if text == "":
+text = input("Ievadi tekstu (atstāj tukšu priekš piemēra): ").strip()
+if not text:
     text = "Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēgas. Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."
     print(text)
 
-# analizē textu izmantojot funkciju biezzums
 atbilde = biezzums(text)
 
-# Izprintē rezultātus
 for word, count in atbilde.items():
     print(f"{word}: {count}")
