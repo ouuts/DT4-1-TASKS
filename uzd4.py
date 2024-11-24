cbb7264c0c58564c6ec25fbae3602a61ed4a979f
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 22:11:01 +0200

    Update uzd4.py

--- uzd4.py
+++ uzd4.py
@@ -5,23 +5,24 @@ from deep_translator import GoogleTranslator
 nltk.download('vader_lexicon')
 sia = SentimentIntensityAnalyzer()
 
+def translate_and_analyze(text):
+
+    translated_text = GoogleTranslator(source='lv', target='en').translate(text)
+    print(f"Tulkojums uz angļu: {translated_text}")
+    sentiment_dict = sia.polarity_scores(translated_text)
+    compound = sentiment_dict['compound']
+    if compound >= 0.05:
+        sentiment = "Pozitīvs"
+    elif compound <= -0.05:
+        sentiment = "Negatīvs"
+    else:
+        sentiment = "Neitrāls"
+    print(f"Emocionālais noskaņojums: {sentiment}")
 while True:
-    teksts = str(input("Ievadiet tekstu (raksti 'atā', lai pārtrauktu): "))
+    text = input("Ievadiet tekstu (raksti 'atā', lai pārtrauktu): ").strip()
     
-    if teksts.lower() in ["atā", "ata"]:
+    if text.lower() in ["atā", "ata"]:
         print("Uz redzēšanos!")
         break
-
-    text = GoogleTranslator(source='lv', target='en').translate(teksts)
-    print(f"Tulkojums uz angļu: {text}")
     
-    sentiment_dict = sia.polarity_scores(text)
-    compound = sentiment_dict['compound']
-    
-    if compound >= 0.05:
-        print("Emocionālais noskaņojums: Pozitīvs")
-    elif compound <= -0.05:
-        print("Emocionālais noskaņojums: Negatīvs")
-    else:
-        print("Emocionālais noskaņojums: Neitrāls")
-    print()
+    translate_and_analyze(text)
