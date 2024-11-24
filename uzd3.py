bccb04924b642ecd8b68790887de91e95cbf4913
ouuts <100085872+ouuts@users.noreply.github.com>
2024-11-24 23:15:39 +0200

    Update uzd3.py

--- uzd3.py
+++ uzd3.py
@@ -1,15 +1,12 @@
 import spacy
 
-# Ielāde valodas moduli. Diemžēl spacy nav lv modulis, bet tāpat strādā
 nlp = spacy.load("en_core_web_md")
 
 text1 = input("Ievadiet pirmo tekstu: ").strip()
 text2 = input("Ievadiet otro tekstu: ").strip()
 
-# Pārbauda tekstus
 doc1 = nlp(text1)
 doc2 = nlp(text2)
 
-# Aprēķina līdzību
 similarity = doc1.similarity(doc2)
 print(f"Līdzība: {similarity * 100:.2f}%")
